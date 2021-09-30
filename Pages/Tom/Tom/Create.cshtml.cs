ed3c98ee9dd765979faa987bccaf7f86ea38afc7
farrert <farrert@mail.nmc.edu>
2021-09-30 12:37:59 -0400

    Add files via upload

---
+++ Pages/Tom/Tom/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using FrisbeeGolfCourseMap.Data;
+using FrisbeeGolfCourseMap.Models;
+
+namespace FrisbeeGolfCourseMap.Pages.Tom
+{
+    public class CreateModel : PageModel
+    {
+        private readonly FrisbeeGolfCourseMap.Data.FrisbeeGolfCourseMapContext _context;
+
+        public CreateModel(FrisbeeGolfCourseMap.Data.FrisbeeGolfCourseMapContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Course Course { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Course.Add(Course);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Pages/Tom/Tom/Index.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FrisbeeGolfCourseMap.Data;
+using FrisbeeGolfCourseMap.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FrisbeeGolfCourseMap.Pages.Tom
+{
+    public class IndexModel : PageModel
+    {
+        private readonly FrisbeeGolfCourseMap.Data.FrisbeeGolfCourseMapContext _context;
+
+        public IndexModel(FrisbeeGolfCourseMap.Data.FrisbeeGolfCourseMapContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> Course { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        //public SelectList Difficulty { get; set; }
+        //[BindProperty(SupportsGet = true)]
+        //public string CourseDifficulty { get; set; }
+        public string NameSort { get; set; }
+        public async Task OnGetAsync(string sortOrder)
+        {
+            NameSort = sortOrder;
+
+            var course = from c in _context.Course
+                         select c;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                course = course.Where(c => c.Name.Contains(SearchString));
+            }
+            // if(!string.IsNullOrEmpty(CourseDifficulty))
+            // {
+            //     course = course.Where(d => d.Difficulty == (CourseDifficulty));
+            //}
+            //Difficulty = new SelectList(await difficultyQuery.Distinct().ToListAsync());
+
+
+            if (NameSort == "desc")
+            {
+                course = course.OrderByDescending(c => c.Name);
+            }
+            else
+            {
+                course = course.OrderBy(c => c.Name);
+            }
+
+            Course = await course.ToListAsync();
+        }
+    }
+}
